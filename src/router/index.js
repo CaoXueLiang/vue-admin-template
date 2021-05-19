daa8d5d68dfbad8bfc34165e859182b0b5752a73
xueliang cao <2357747972@qq.com>
2021-05-19 11:03:22 +0800

    备份

--- src/router/index.js
+++ src/router/index.js
@@ -1,176 +1,169 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 
 Vue.use(Router);
 
-/* Layout */
-import Layout from "@/layout";
+import Layout from '@/layout';
+const Login = () => import('@/views/login/index');
+const errorPage = () => import('@/views/404');
+const Dashboard = () => import('@/views/dashboard/index');
+const Table = () => import('@/views/table/index');
+const Tree = () => import('@/views/tree/index');
+const buttonPage = () => import('@/views/buttons/buttons');
+const Form = () => import('@/views/form/index');
+const menuOne = () => import('@/views/nested/menu1/index');
+const menuOneOne = () => import('@/views/nested/menu1/menu1-1');
+const menuOneTwo = () => import('@/views/nested/menu1/menu1-2');
+const menuOneTwoOne = () => import('@/views/nested/menu1/menu1-2/menu1-2-1');
+const menuOneTwoTwo = () => import('@/views/nested/menu1/menu1-2/menu1-2-2');
+const menuOneTree = () => import('@/views/nested/menu1/menu1-3');
+const menuTwo = () => import('@/views/nested/menu2/index');
 
-/**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
-/**
- * constantRoutes
- * a base page that does not have permission requirements
- * all roles can be accessed
- */
 export const constantRoutes = [
   {
-    path: "/login",
-    component: () => import("@/views/login/index"),
+    path: '/login',
+    component: Login,
     hidden: true
   },
 
   {
-    path: "/404",
-    component: () => import("@/views/404"),
+    path: '/404',
+    component: errorPage,
     hidden: true
   },
 
   {
-    path: "/",
+    path: '/',
     component: Layout,
-    redirect: "/dashboard",
+    redirect: '/dashboard',
     children: [
       {
-        path: "dashboard",
-        name: "Dashboard",
-        component: () => import("@/views/dashboard/index"),
-        meta: { title: "首页", icon: "dashboard" }
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+        meta: { title: '首页', icon: 'dashboard' }
       }
     ]
   },
 
   {
-    path: "/example",
+    path: '/example',
     component: Layout,
-    redirect: "/example/table",
-    name: "Example",
-    meta: { title: "示例", icon: "el-icon-s-help" },
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: '示例', icon: 'el-icon-s-help' },
     children: [
       {
-        path: "table",
-        name: "Table",
-        component: () => import("@/views/table/index"),
-        meta: { title: "表格", icon: "table" }
+        path: 'table',
+        name: 'Table',
+        component: Table,
+        meta: { title: '表格', icon: 'table' }
       },
       {
-        path: "tree",
-        name: "Tree",
-        component: () => import("@/views/tree/index"),
-        meta: { title: "树", icon: "tree" }
+        path: 'tree',
+        name: 'Tree',
+        component: Tree,
+        meta: { title: '树', icon: 'tree' }
+      },
+      {
+        path: 'buttons',
+        name: 'Buttons',
+        component: buttonPage,
+        meta: { title: '按钮', icon: 'tree' }
       }
     ]
   },
 
   {
-    path: "/form",
+    path: '/form',
     component: Layout,
     children: [
       {
-        path: "index",
-        name: "Form",
-        component: () => import("@/views/form/index"),
-        meta: { title: "表单", icon: "form" }
+        path: 'index',
+        name: 'Form',
+        component: Form,
+        meta: { title: '表单', icon: 'form' }
       }
     ]
   },
 
   {
-    path: "/nested",
+    path: '/nested',
     component: Layout,
-    redirect: "/nested/menu1",
-    name: "Nested",
+    redirect: '/nested/menu1',
+    name: 'Nested',
     meta: {
-      title: "路由嵌套",
-      icon: "nested"
+      title: '路由嵌套',
+      icon: 'nested'
     },
     children: [
       {
-        path: "menu1",
-        component: () => import("@/views/nested/menu1/index"), // Parent router-view
-        name: "Menu1",
-        meta: { title: "菜单1" },
+        path: 'menu1',
+        component: menuOne,
+        name: 'Menu1',
+        meta: { title: '菜单1' },
         children: [
           {
-            path: "menu1-1",
-            component: () => import("@/views/nested/menu1/menu1-1"),
-            name: "Menu1-1",
-            meta: { title: "菜单1-1" }
+            path: 'menu1-1',
+            component: menuOneOne,
+            name: 'Menu1-1',
+            meta: { title: '菜单1-1' }
           },
           {
-            path: "menu1-2",
-            component: () => import("@/views/nested/menu1/menu1-2"),
-            name: "Menu1-2",
-            meta: { title: "菜单1-2" },
+            path: 'menu1-2',
+            component: menuOneTwo,
+            name: 'Menu1-2',
+            meta: { title: '菜单1-2' },
             children: [
               {
-                path: "menu1-2-1",
-                component: () =>
-                  import("@/views/nested/menu1/menu1-2/menu1-2-1"),
-                name: "Menu1-2-1",
-                meta: { title: "菜单1-2-1" }
+                path: 'menu1-2-1',
+                component: menuOneTwoOne,
+                name: 'Menu1-2-1',
+                meta: { title: '菜单1-2-1' }
               },
               {
-                path: "menu1-2-2",
-                component: () =>
-                  import("@/views/nested/menu1/menu1-2/menu1-2-2"),
-                name: "Menu1-2-2",
-                meta: { title: "菜单1-2-2" }
+                path: 'menu1-2-2',
+                component: menuOneTwoTwo,
+                name: 'Menu1-2-2',
+                meta: { title: '菜单1-2-2' }
               }
             ]
           },
           {
-            path: "menu1-3",
-            component: () => import("@/views/nested/menu1/menu1-3"),
-            name: "Menu1-3",
-            meta: { title: "菜单1-3" }
+            path: 'menu1-3',
+            component: menuOneTree,
+            name: 'Menu1-3',
+            meta: { title: '菜单1-3' }
           }
         ]
       },
       {
-        path: "menu2",
-        component: () => import("@/views/nested/menu2/index"),
-        name: "Menu2",
-        meta: { title: "菜单2" }
+        path: 'menu2',
+        component: menuTwo,
+        name: 'Menu2',
+        meta: { title: '菜单2' }
       }
     ]
   },
 
   {
-    path: "external-link",
+    path: 'external-link',
     component: Layout,
     children: [
       {
-        path: "https://panjiachen.github.io/vue-element-admin-site/#/",
-        meta: { title: "外部链接", icon: "link" }
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: '外部链接', icon: 'link' }
       }
     ]
   },
 
   // 404 page must be placed at the end !!!
-  { path: "*", redirect: "/404", hidden: true }
+  { path: '*', redirect: '/404', hidden: true }
 ];
 
 const createRouter = () =>
   new Router({
-    mode: "history",
+    mode: 'history',
     scrollBehavior: () => ({ y: 0 }),
     routes: constantRoutes
   });
